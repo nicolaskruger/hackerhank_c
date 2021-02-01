c0fbfe3d3fa75d21eab2c26c9a2981ce547ad64c
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-02-01 16:11:08 -0300

    finish

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ int main(int argc, char const *argv[])
 {
     for (size_t i = 0; i < 3; i++)
     {
-        char ch[100];
-        scanf("%[^\n]c",&ch);
+         char ch[100];
+        scanf("%[^\n]%*c",&ch);
         printf("%s\n",ch);
     }
     
