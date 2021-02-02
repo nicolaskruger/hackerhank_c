62026ab39635335b369643b6a0a782c0f6922f8d
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-02-02 15:13:11 -0300

    finish

--- main.c
+++ main.c
@@ -3,24 +3,18 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
+int main() {
 
-int main() 
-{
-
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int n;
-    scanf("%d", &n);
-  	// Complete the code to print the pattern.
-    const int N = 2*n+1;
-    for (size_t j = 1; j < N-1; j++)
-        for (size_t i = 1; i < N-1; i++){
-            printf("%d ",max(abs(n-j),abs(n-i))+1);
-            if(i==(N-2))
-                printf("\n");
-        }
-    
+    int sum=0;
+    int buff;
+    scanf("%d",&n);
+    for (size_t i = 0; i < n; i++)
+    {
+        scanf("%d",&buff);
+        sum+=buff;
+    }
+    printf("%d",sum);
     return 0;
 }
