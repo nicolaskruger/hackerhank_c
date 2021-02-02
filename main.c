cf25a198f653a4896ea8663b0f3d82d2ba02d425
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-02-02 13:11:13 -0300

    finish

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+void update(int *a,int *b) {
+    // Complete this function
+    int res[2];
+    res[0] = (*a) + (*b);
+    res[1] = abs((*a) - (*b));
+    *a = res[0];
+    *b = res[1];    
+}
 
-int main(int argc, char const *argv[])
-{
-    printf("hellow");
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
     return 0;
 }
